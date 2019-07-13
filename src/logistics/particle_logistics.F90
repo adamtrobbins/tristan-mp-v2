d1c44e38ca178885111bb29d9dd2ff37e924a1a2
haykh <hayk.hakopyan@gmail.com>
2019-07-13 15:44:27 -0400

    minor bugfix

--- src/logistics/particle_logistics.F90
+++ src/logistics/particle_logistics.F90
@@ -61,7 +61,7 @@ contains
         & (yi .lt. species(s)%prtl_tile(ti, tj, tk)%y1) .or. &
         & (yi .ge. species(s)%prtl_tile(ti, tj, tk)%y2) .or. &
         & (zi .lt. species(s)%prtl_tile(ti, tj, tk)%z1) .or. &
-        & (zi .ge. species(s)%prtl_tile(ti, tj, tk)%z2) .or. &) then
+        & (zi .ge. species(s)%prtl_tile(ti, tj, tk)%z2)) then
         call throwError('ERROR: wrong ti, tj, tk in `createParticle` according to x1,x2,etc')
       end if
     #endif
