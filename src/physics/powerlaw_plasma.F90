c500bf6bd9304aafe732bf93ef27d678d2c93e3a
haykh <hayk.hakopyan@gmail.com>
2019-09-11 16:59:26 -0400

    fillregion now uses global coords

--- src/physics/powerlaw_plasma.F90
+++ src/physics/powerlaw_plasma.F90
@@ -26,8 +26,11 @@ contains
     real, intent(in)                 :: ndens_sp, plaw_gmin, plaw_gmax, plaw_ind
     integer                          :: num_part, n, s, spec_
     integer(kind=2)                  :: xi_, yi_, zi_
+    real                             :: fill_xmin, fill_xmax,&
+                                      & fill_ymin, fill_ymax,&
+                                      & fill_zmin, fill_zmax
     real                             :: u_, v_, w_, dx_, dy_, dz_
-    real                             :: x_, y_, z_, gam_, bet_, TH, ZT, rnd
+    real                             :: x_, y_, z_, gam_, bet_, TH, ZT, rnd, num_part_r
     real                             :: x_glob, y_glob, z_glob
 
     procedure (spatialDistribution), pointer, intent(in), optional :: spat_distr_ptr
@@ -51,38 +54,48 @@ contains
     end if
     
     ! global to local coordinates
-    fill_region%x_min = MAX(0.0, fill_region%x_min - REAL(this_meshblock%ptr%x0))
-    fill_region%x_max = MIN(REAL(this_meshblock%ptr%sx),&
+    fill_xmin = MAX(0.0, fill_region%x_min - REAL(this_meshblock%ptr%x0))
+    fill_xmax = MIN(REAL(this_meshblock%ptr%sx),&
                           & fill_region%x_max - REAL(this_meshblock%ptr%x0))
-    fill_region%y_min = MAX(0.0, fill_region%y_min - REAL(this_meshblock%ptr%y0))
-    fill_region%y_max = MIN(REAL(this_meshblock%ptr%sy),&
+    fill_ymin = MAX(0.0, fill_region%y_min - REAL(this_meshblock%ptr%y0))
+    fill_ymax = MIN(REAL(this_meshblock%ptr%sy),&
                           & fill_region%y_max - REAL(this_meshblock%ptr%y0))
     #ifdef threeD
-      fill_region%z_min = MAX(0.0, fill_region%z_min - REAL(this_meshblock%ptr%z0))
-      fill_region%z_max = MIN(REAL(this_meshblock%ptr%sz),&
+      fill_zmin = MAX(0.0, fill_region%z_min - REAL(this_meshblock%ptr%z0))
+      fill_zmax = MIN(REAL(this_meshblock%ptr%sz),&
                             & fill_region%z_max - REAL(this_meshblock%ptr%z0))
     #endif
 
     #ifndef threeD
-      num_part = INT(ndens_sp * (fill_region%x_max - fill_region%x_min)&
-                            & * (fill_region%y_max - fill_region%y_min))
+      num_part_r = REAL(ndens_sp) * (fill_xmax - fill_xmin)&
+                                & * (fill_ymax - fill_ymin)
     #else 
-      num_part = INT(ndens_sp * (fill_region%x_max - fill_region%x_min)&
-                            & * (fill_region%y_max - fill_region%y_min)&
-                            & * (fill_region%z_max - fill_region%z_min))
+      num_part_r = REAL(ndens_sp) * (fill_xmax - fill_xmin)&
+                                & * (fill_ymax - fill_ymin)&
+                                & * (fill_zmax - fill_zmin))
     #endif
+    if (num_part_r .lt. 10.0) then
+      if (num_part_r .ne. 0.0) then
+        num_part_r = poisson(num_part_r)
+      else
+        num_part_r = 0.0
+      end if
+    else
+      num_part_r = CEILING(num_part_r)
+    end if
+    num_part = INT(num_part_r)
 
     n = 0
     do while (n .lt. num_part)
       ! generate coords for all species
       rnd = random(dseed)
-      x_ = fill_region%x_min + rnd * (fill_region%x_max - fill_region%x_min)
+      x_ = fill_xmin + rnd * (fill_xmax - fill_xmin)
       xi_ = INT(FLOOR(x_), 2); dx_ = x_ - FLOOR(x_)
       rnd = random(dseed)
-      y_ = fill_region%y_min + rnd * (fill_region%y_max - fill_region%y_min)
+      y_ = fill_ymin + rnd * (fill_ymax - fill_ymin)
       yi_ = INT(FLOOR(y_), 2); dy_ = y_ - FLOOR(y_)
       #ifdef threeD
-        z_ = fill_region%z_min + random(dseed) * (fill_region%z_max - fill_region%z_min)
+        z_ = fill_zmin + random(dseed) * (fill_zmax - fill_zmin)
         zi_ = INT(FLOOR(z_), 2); dz_ = z_ - FLOOR(z_)
       #else
         z_ = 0.5
