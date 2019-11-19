d47ec37a84c4fc481644a7ba6e2c1c4005839964
haykh <hayk.hakopyan@gmail.com>
2019-11-19 16:54:24 -0500

    testing coupling

--- src/tristan.F90
+++ src/tristan.F90
@@ -4,11 +4,28 @@ program tristan
   use m_initialize
   use m_mainloop
   use m_finalize
+
+    use m_bincoupling
+
   implicit none
   !----- main code --------------------------
+  integer :: num_couples, i
+  type(couple), allocatable, intent(out)  :: coupled_pairs
 
   call initializeAll()
-  call mainloop()
+
+  call coupleParticlesOnTile(0, 0, 0,&
+                           & (/1/), 1,&
+                           & (/2/), 1,&
+                           & coupled_pairs, num_couples)
+
+  if (this_meshblock%ptr%rnk .eq. 0) then
+    print *, "num pairs:", num_couples
+    do i = 1, num_couples
+      print *, i, coupled_pairs(i)%part_1%spec, coupled_pairs(i)%part_1%index, coupled_pairs(i)%part_2%spec, coupled_pairs(i)%part_2%index
+    end do
+  end if
+  ! call mainloop()
   call finalizeAll()
 
   !..... main code ..........................
