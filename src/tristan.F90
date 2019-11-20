deb650026db61882a7acaaf139d86260965f0224
haykh <hayk.hakopyan@gmail.com>
2019-11-20 14:40:00 -0500

    fix

--- src/tristan.F90
+++ src/tristan.F90
@@ -16,13 +16,13 @@ program tristan
 
   call coupleParticlesOnTile(1, 1, 1,&
                            & (/1/), 1,&
-                           & (/1/), 1,&
+                           & (/2/), 1,&
                            & coupled_pairs, num_couples)
 
   if (this_meshblock%ptr%rnk .eq. 0) then
     print *, "num pairs:", num_couples
     print *, "num spec1:", species(1)%prtl_tile(1, 1, 1)%npart_sp
-    ! print *, "num spec2:", species(2)%prtl_tile(1, 1, 1)%npart_sp
+    print *, "num spec2:", species(2)%prtl_tile(1, 1, 1)%npart_sp
     do i = 1, num_couples
       print *, i, coupled_pairs(i)%part_1%spec, coupled_pairs(i)%part_1%index, coupled_pairs(i)%part_2%spec, coupled_pairs(i)%part_2%index
     end do
