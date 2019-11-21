681f8710f5075b4c96ee076d3f8044ba18be1ea2
haykh <hayk.hakopyan@gmail.com>
2019-11-21 18:14:50 -0500

    added new BW file and a QED file

---
+++ src/physics/qed_physics.F90
@@ -0,0 +1,19 @@
+#include "../defs.F90"
+
+module m_qedphysics
+  use m_globalnamespace
+  use m_bwpairproduction
+  implicit none
+
+  !--- PRIVATE variables/functions -------------------------------!
+  !...............................................................!
+contains
+  subroutine QEDstep(timestep)
+    implicit none
+    integer, intent(in) :: timestep
+    if (modulo(timestep, BW_interval) .eq. 0) then
+      call bwPairProduction()
+    end if
+  end subroutine QEDstep
+
+end module m_qedphysics
