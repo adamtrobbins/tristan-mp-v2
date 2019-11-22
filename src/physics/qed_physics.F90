60bbfd48e923ff9230b511753625460c96b5cfe7
haykh <hayk.hakopyan@gmail.com>
2019-11-21 20:51:54 -0500

    minor bug fixed

--- src/physics/qed_physics.F90
+++ src/physics/qed_physics.F90
@@ -3,6 +3,7 @@
 module m_qedphysics
 #ifdef QED
 
+  use m_aux
   use m_globalnamespace
   use m_bwpairproduction
   implicit none
@@ -16,6 +17,7 @@ contains
     if (modulo(timestep, BW_interval) .eq. 0) then
       call bwPairProduction()
     end if
+    call printDiag((mpi_rank .eq. 0), "QEDstep()", .true.)
   end subroutine QEDstep
 
 #endif
