e7a5488578845a5d4ef4d6064340c4bb174b2e6c
haykh <hayk.hakopyan@gmail.com>
2019-11-21 19:13:34 -0500

    fixed user bulb

--- user/user_two_bulbs.F90
+++ user/user_two_bulbs.F90
@@ -167,7 +167,7 @@ contains
           yi_ = yi_ - 1; dy_ = dy_ + 1.0
         end if
 
-        call createParticle(1, xi_, yi_, yi, dx_, dy_, dz_, u_, v_, w_)
+        call createParticle(1, xi_, yi_, yi_, dx_, dy_, dz_, u_, v_, w_)
       end do
     end if
 
