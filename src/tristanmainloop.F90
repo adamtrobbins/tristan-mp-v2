382c3c03df82b3a98768493bdad2ff87ff0e5286
haykh <hayk.hakopyan@gmail.com>
2020-01-02 17:12:27 -0500

    fixed makefile

--- src/tristanmainloop.F90
+++ src/tristanmainloop.F90
@@ -148,7 +148,7 @@ contains
       !-------------------------------------------------
       ! Depositing current: `j_s = rho_s * v_s`
         t_depositstep = MPI_WTIME()
-      call depositCurrents()
+      ! call depositCurrents()
         t_depositstep = MPI_WTIME() - t_depositstep
       !.................................................
 
