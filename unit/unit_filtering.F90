d4a7a58c1b15bdc20d214a97c62ca3b893c23130
haykh <hayk.hakopyan@gmail.com>
2020-01-02 17:56:28 -0500

    fixed makefile

--- unit/unit_filtering.F90
+++ unit/unit_filtering.F90
@@ -66,7 +66,11 @@ contains
     integer :: i_glob, j_glob, k_glob
     ex(:,:,:) = 0; ey(:,:,:) = 0; ez(:,:,:) = 0
     bx(:,:,:) = 0; by(:,:,:) = 0; bz(:,:,:) = 0
-    jx(:,:,:) = 0; jy(:,:,:) = 0; jz(:,:,:) = 1.0
+    jx(:,:,:) = 0; jy(:,:,:) = 0;
+    if (mpi_rank .eq. 0) then
+      jz(:,:,:) = 1.0
+    else
+      jz(:,:,:) = 0.0
     ! ... dummy loop ...
     ! do i = 0, this_meshblock%ptr%sx - 1
     !   i_glob = i + this_meshblock%ptr%x0
