ab140c65ed502be213ba2b85937931f56031ba54
haykh <hayk.hakopyan@gmail.com>
2020-01-19 01:23:28 -0500

    minimizing expressions

--- src/tools/interp_bfield.F90
+++ src/tools/interp_bfield.F90
@@ -29,34 +29,19 @@
 ! c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
 ! bz0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
 
+bx0 = 0.5 * ((1.0 - pt_dy(p)) * ((1.0 - pt_dx(p)) * (bx(lind,1,1) + bx(lind-mx,1,1))) +&
+        & pt_dx(p) * (bx(lind+1,1,1) + bx(lind+1-mx,1,1))) +&
+        & pt_dy(p) * ((1.0 - pt_dx(p)) * (bx(lind,1,1) + bx(lind + mx,1,1)) +&
+        & pt_dx(p) * (bx(lind+1,1,1) + bx(lind+1+mx,1,1))))
 
-!b_x
-c000 = 0.5 * (bx(lind, 1, 1) + bx(lind_O_M, 1, 1))
-c100 = 0.5 * (bx(lind_P_O, 1, 1) + bx(lind_P_M, 1, 1))
-c010 = 0.5 * (bx(lind, 1, 1) + bx(lind_O_P, 1, 1))
-c110 = 0.5 * (bx(lind_P_O, 1, 1) + bx(lind_P_P, 1, 1))
-c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
-c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
-bx0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+by0 = 0.5 * ((1.0 - pt_dy(p)) * ((1.0 - pt_dx(p)) * by(,1,1) - by(lind,1,1) - pt_dx(p)*by(lind+1,1,1)) +&
+        & pt_dy(p) * (((1.0 - pt_dx(p)) * by(lind-1+mx,1,1)) + by(lind+mx,1,1) +&
+        & pt_dx(p) * by(lind+1+mx,1,1)))
 
-!b_y
-c000 = 0.5 * (by(lind_M_O, 1, 1) + by(lind, 1, 1))
-c100 = 0.5 * (by(lind, 1, 1) + by(lind_P_O, 1, 1))
-c010 = 0.5 * (by(lind_M_P, 1, 1) + by(lind_O_P, 1, 1))
-c110 = 0.5 * (by(lind_O_P, 1, 1) + by(lind_P_P, 1, 1))
-c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
-c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
-by0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
-
-!b_z
-c000 = 0.25 * (bz(lind_M_M, 1, 1) + bz(lind_M_O, 1, 1) + &
-              &bz(lind_O_M, 1, 1) + bz(lind, 1, 1))
-c100 = 0.25 * (bz(lind_O_M, 1, 1) + bz(lind, 1, 1) + &
-              &bz(lind_P_M, 1, 1) + bz(lind_P_O, 1, 1))
-c010 = 0.25 * (bz(lind_M_O, 1, 1) + bz(lind_M_P, 1, 1) + &
-              &bz(lind, 1, 1) + bz(lind_O_P, 1, 1))
-c110 = 0.25 * (bz(lind, 1, 1) + bz(lind_O_P, 1, 1) + &
-              &bz(lind_P_O, 1, 1) + bz(lind_P_P, 1, 1))
-c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
-c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
-bz0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+bz0 = 0.25 * ((1.0 - pt_dy(p)) * (((1.0 - pt_dx(p)) * (bz(lind-1,1,1) + bz(lind,1,1) +&
+        & bz(lind-1-mx,1,1) + bz(lind-mx,1,1))) +&
+        & pt_dx(p) * (bz(lind,1,1) + bz(lind+1,1,1) + bz(lind-mx,1,1) +&
+          & bz(lind+1-mx,1,1))) +&
+        & pt_dy(p) * (-((-1 + pt_dx(p)) * bz(lind-1,1,1)) + bz(lind,1,1) + pt_dx(p) * bz(lind+1,1,1) +&
+          & bz(lind-1+mx,1,1) - pt_dx(p) * bz(lind-1+mx,1,1) + bz(lind+mx,1,1) +&
+          & pt_dx(p) * bz(lind+1+mx,1,1)))
