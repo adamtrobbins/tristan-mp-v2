ca5d9424bc70f2d1a37eae4d279aa426af4ee998
haykh <hayk.hakopyan@gmail.com>
2020-01-20 00:31:05 -0500

    checking inlined funcs

--- src/tools/interp_bfield.F90
+++ src/tools/interp_bfield.F90
@@ -1,36 +1,64 @@
 ! linear indices used isntead of 3d
 
 #ifndef threeD
-  !b_x
-  c000 = 0.5 * (bx(lind, -NGHOST, 0) + bx(lind - iy, -NGHOST, 0))
-  c100 = 0.5 * (bx(lind + 1, -NGHOST, 0) + bx(lind + 1 - iy, -NGHOST, 0))
-  c010 = 0.5 * (bx(lind, -NGHOST, 0) + bx(lind + iy, -NGHOST, 0))
-  c110 = 0.5 * (bx(lind + 1, -NGHOST, 0) + bx(lind + iy + 1, -NGHOST, 0))
-  c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
-  c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
-  bx0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+  ! !b_x
+  ! c000 = 0.5 * (bx(lind, -NGHOST, 0) + bx(lind - iy, -NGHOST, 0))
+  ! c100 = 0.5 * (bx(lind + 1, -NGHOST, 0) + bx(lind + 1 - iy, -NGHOST, 0))
+  ! c010 = 0.5 * (bx(lind, -NGHOST, 0) + bx(lind + iy, -NGHOST, 0))
+  ! c110 = 0.5 * (bx(lind + 1, -NGHOST, 0) + bx(lind + iy + 1, -NGHOST, 0))
+  ! c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
+  ! c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
+  ! bx0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+  !
+  ! !b_y
+  ! c000 = 0.5 * (by(lind - 1, -NGHOST, 0) + by(lind, -NGHOST, 0))
+  ! c100 = 0.5 * (by(lind, -NGHOST, 0) + by(lind + 1, -NGHOST, 0))
+  ! c010 = 0.5 * (by(lind + iy - 1, -NGHOST, 0) + by(lind + iy, -NGHOST, 0))
+  ! c110 = 0.5 * (by(lind + iy, -NGHOST, 0) + by(lind + iy + 1, -NGHOST, 0))
+  ! c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
+  ! c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
+  ! by0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+  !
+  ! !b_z
+  ! c000 = 0.25 * (bz(lind - 1 - iy, -NGHOST, 0) + bz(lind - 1, -NGHOST, 0) + &
+  !               &bz(lind - iy, -NGHOST, 0) + bz(lind, -NGHOST, 0))
+  ! c100 = 0.25 * (bz(lind - iy, -NGHOST, 0) + bz(lind, -NGHOST, 0) + &
+  !               &bz(lind + 1 - iy, -NGHOST, 0) + bz(lind + 1, -NGHOST, 0))
+  ! c010 = 0.25 * (bz(lind - 1, -NGHOST, 0) + bz(lind + iy - 1, -NGHOST, 0) + &
+  !               &bz(lind, -NGHOST, 0) + bz(lind + iy, -NGHOST, 0))
+  ! c110 = 0.25 * (bz(lind, -NGHOST, 0) + bz(lind + iy, -NGHOST, 0) + &
+  !               &bz(lind + 1, -NGHOST, 0) + bz(lind + iy + 1, -NGHOST, 0))
+  ! c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
+  ! c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
+  ! bz0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+  c000=0.5*(bx(pt_xi(p),pt_yi(p),pt_zi(p))+bx(pt_xi(p),pt_yi(p)-1,pt_zi(p)))
+  c100=0.5*(bx(pt_xi(p)+1,pt_yi(p),pt_zi(p))+bx(pt_xi(p)+1,pt_yi(p)-1,pt_zi(p)))
+  c010=0.5*(bx(pt_xi(p),pt_yi(p),pt_zi(p))+bx(pt_xi(p),pt_yi(p)+1,pt_zi(p)))
+  c110=0.5*(bx(pt_xi(p)+1,pt_yi(p),pt_zi(p))+bx(pt_xi(p)+1,pt_yi(p)+1,pt_zi(p)))
+  c00=c000*(1.0-pt_dx(p))+c100*pt_dx(p)
+  c10=c010*(1.0-pt_dx(p))+c110*pt_dx(p)
+  bx0=c00*(1.0-pt_dy(p))+c10*pt_dy(p)
 
-  !b_y
-  c000 = 0.5 * (by(lind - 1, -NGHOST, 0) + by(lind, -NGHOST, 0))
-  c100 = 0.5 * (by(lind, -NGHOST, 0) + by(lind + 1, -NGHOST, 0))
-  c010 = 0.5 * (by(lind + iy - 1, -NGHOST, 0) + by(lind + iy, -NGHOST, 0))
-  c110 = 0.5 * (by(lind + iy, -NGHOST, 0) + by(lind + iy + 1, -NGHOST, 0))
-  c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
-  c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
-  by0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+  c000=0.5*(by(pt_xi(p)-1,pt_yi(p),pt_zi(p))+by(pt_xi(p),pt_yi(p),pt_zi(p)))
+  c100=0.5*(by(pt_xi(p),pt_yi(p),pt_zi(p))+by(pt_xi(p)+1,pt_yi(p),pt_zi(p)))
+  c010=0.5*(by(pt_xi(p)-1,pt_yi(p)+1,pt_zi(p))+by(pt_xi(p),pt_yi(p)+1,pt_zi(p)))
+  c110=0.5*(by(pt_xi(p),pt_yi(p)+1,pt_zi(p))+by(pt_xi(p)+1,pt_yi(p)+1,pt_zi(p)))
+  c00=c000*(1.0-pt_dx(p))+c100*pt_dx(p)
+  c10=c010*(1.0-pt_dx(p))+c110*pt_dx(p)
+  by0=c00*(1.0-pt_dy(p))+c10*pt_dy(p)
 
-  !b_z
-  c000 = 0.25 * (bz(lind - 1 - iy, -NGHOST, 0) + bz(lind - 1, -NGHOST, 0) + &
-                &bz(lind - iy, -NGHOST, 0) + bz(lind, -NGHOST, 0))
-  c100 = 0.25 * (bz(lind - iy, -NGHOST, 0) + bz(lind, -NGHOST, 0) + &
-                &bz(lind + 1 - iy, -NGHOST, 0) + bz(lind + 1, -NGHOST, 0))
-  c010 = 0.25 * (bz(lind - 1, -NGHOST, 0) + bz(lind + iy - 1, -NGHOST, 0) + &
-                &bz(lind, -NGHOST, 0) + bz(lind + iy, -NGHOST, 0))
-  c110 = 0.25 * (bz(lind, -NGHOST, 0) + bz(lind + iy, -NGHOST, 0) + &
-                &bz(lind + 1, -NGHOST, 0) + bz(lind + iy + 1, -NGHOST, 0))
-  c00 = c000 * (1 - pt_dx(p)) + c100 * pt_dx(p)
-  c10 = c010 * (1 - pt_dx(p)) + c110 * pt_dx(p)
-  bz0 = c00 * (1 - pt_dy(p)) + c10 * pt_dy(p)
+  c000=0.25*(bz(pt_xi(p)-1,pt_yi(p)-1,pt_zi(p))+bz(pt_xi(p)-1,pt_yi(p),pt_zi(p))+&
+  &bz(pt_xi(p),pt_yi(p)-1,pt_zi(p))+bz(pt_xi(p),pt_yi(p),pt_zi(p)))
+  c100=0.25*(bz(pt_xi(p),pt_yi(p)-1,pt_zi(p))+bz(pt_xi(p),pt_yi(p),pt_zi(p))+&
+  &bz(pt_xi(p)+1,pt_yi(p)-1,pt_zi(p))+bz(pt_xi(p)+1,pt_yi(p),pt_zi(p)))
+  c010=0.25*(bz(pt_xi(p)-1,pt_yi(p),pt_zi(p))+bz(pt_xi(p)-1,pt_yi(p)+1,pt_zi(p))+&
+  &bz(pt_xi(p),pt_yi(p),pt_zi(p))+bz(pt_xi(p),pt_yi(p)+1,pt_zi(p)))
+  c110=0.25*(bz(pt_xi(p),pt_yi(p),pt_zi(p))+bz(pt_xi(p),pt_yi(p)+1,pt_zi(p))+&
+  &bz(pt_xi(p)+1,pt_yi(p),pt_zi(p))+bz(pt_xi(p)+1,pt_yi(p)+1,pt_zi(p)))
+  c00=c000*(1.0-pt_dx(p))+c100*pt_dx(p)
+  c10=c010*(1.0-pt_dx(p))+c110*pt_dx(p)
+  bz0=c00*(1.0-pt_dy(p))+c10*pt_dy(p)
+
 #else
   !b_x
   c000 = 0.25 * (bx(lind, -NGHOST, -NGHOST) + bx(lind - iy, -NGHOST, -NGHOST) +&
