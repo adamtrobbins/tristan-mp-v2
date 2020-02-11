802030a50f41d59b6fbaec6814150051ed63b792
haykh <hayk.hakopyan@gmail.com>
2020-02-11 13:48:51 -0500

    new files for particle downsampling support

--- src/logistics/particle_downsampling.F90
+++ src/logistics/particle_downsampling.F90
@@ -1,12 +1,78 @@
 #include "../defs.F90"
 
 module m_particledownsampling
+#ifdef DOWNSAMPLING
+
   use m_globalnamespace
   use m_aux
   use m_helpers
   use m_errors
   use m_domain
   use m_particles
+  use m_particlelogistics
+  use m_momentumbinning
+  implicit none
+
+  real              :: dwn_interval
+  integer           :: dwn_interval
+
+  !--- PRIVATE variables/functions -------------------------------!
+  private :: downsampleParticles, downsampleOnTile,&
+           & downsampleBinnedParticles
+  !...............................................................!
 contains
-  
+  subroutine downsamplingStep(timestep)
+    implicit none
+    integer, intent(in) :: timestep
+    if (modulo(timestep, dwn_interval) .eq. 0) then
+      call downsampleParticles()
+    end if
+    call printDiag((mpi_rank .eq. 0), "downsamplingStep()", .true.)
+  end subroutine downsamplingStep
+
+  subroutine downsampleParticles()
+    implicit none
+    integer :: s, ti, tj, tk
+
+    do s = 1, nspec
+      if (species(s)%dwn_sp) then
+        do ti = 1, species(s)%tile_nx
+          do tj = 1, species(s)%tile_ny
+            do tk = 1, species(s)%tile_nz
+              call downsampleOnTile(s, ti, tj, tk)
+            end do
+          end do
+        end do
+      end if
+    end do
+
+  end subroutine downsampleParticles
+
+  subroutine downsampleOnTile(s, ti, tj, tk)
+    implicit none
+    integer, intent(in) :: s, ti, tj, tk
+    ! particles are now binned into `energy_bins`
+    call binParticlesOnTile(species(s)%prtl_tile(ti, tj, tk))
+    call downsampleBinnedParticles()
+    call deinitializeEnergyBins()
+  end subroutine downsampleOnTile
+
+  subroutine downsampleBinnedParticles(s, ti, tj, tk)
+    implicit none
+    integer, intent(in) :: s, ti, tj, tk
+    integer             :: e_b, th_b, ph_b, p_ind, p, npart
+    do e_b = 0, n_energy_bins - 1
+      do th_b = 0, energy_bins(e_b)%n_theta_bins
+        do ph_b = 0, energy_bins(e_b)%theta_bins(th_b)%n_phi_bins
+          npart = energy_bins(e_b)%theta_bins(th_b)%phi_bins(ph_b)%npart
+          do p_ind = 1, npart
+            p = energy_bins(e_b)%theta_bins(th_b)%phi_bins(ph_b)%indices(p_ind)
+            species(s)%prtl_tile(ti, tj, tk)%ind = p_ind + 100 * ph_b + 100**2 * e_b
+          end do
+        end do
+      end do
+    end do
+  end subroutine downsampleBinnedParticles
+
+#endif
 end module m_particledownsampling
