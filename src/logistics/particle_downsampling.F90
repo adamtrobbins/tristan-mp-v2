a9e9aea5c3e022c8ac233b33c41365575af42626
haykh <hayk.hakopyan@gmail.com>
2020-02-12 17:56:33 -0500

    testing

--- src/logistics/particle_downsampling.F90
+++ src/logistics/particle_downsampling.F90
@@ -83,6 +83,7 @@ contains
             #ifdef DEBUG
               if ((p .le. 0) .or. (p .gt. tile%npart_sp)) then
                 print *, p, tile%npart_sp, npart
+                print *, e_b, th_b, ph_b
                 call throwError('Something went terribly wrong during the binning.')
               end if
 
