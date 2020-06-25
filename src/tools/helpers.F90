f0fe9c415085e58cfe543cdd770576f491ee93a8
haykh <hayk.hakopyan@gmail.com>
2020-06-25 03:24:01 -0400

    moved field preparing routine for output outside (reused for slice)

--- src/tools/helpers.F90
+++ src/tools/helpers.F90
@@ -3,6 +3,7 @@
 module m_helpers
   use m_globalnamespace
   use m_aux
+  use m_errors
   use m_domain
   use m_particles
   use m_fields
@@ -695,4 +696,93 @@ contains
       intfz = c0 * (1 - dz) + c1 * dz
     #endif
   end subroutine interpFromFaces
+
+  ! writes a field specified by `fld_var` from gridcell `i,j,k` ...
+  ! ... to `sm_arr(i1, j1, k1)` with proper interpolation etc for the output
+  subroutine selectFieldForOutput(fld_var, i1, j1, k1, i, j, k, writing_lgarrQ)
+    implicit none
+    character(len=*), intent(in)  :: fld_var
+    integer(kind=2), intent(in)   :: i1, j1, k1, i, j, k
+    logical, intent(in)           :: writing_lgarrQ
+    real                          :: ex0, ey0, ez0, bx0, by0, bz0, jx0, jy0, jz0
+    select case (trim(fld_var))
+    case('ex')
+      #ifndef debug
+        call interpFromEdges(0.0, 0.0, 0.0, i, j, k, ex, ey, ez, ex0, ey0, ez0)
+      #else
+        ex0 = ex(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = ex0 * B_norm
+    case('ey')
+      #ifndef debug
+        call interpFromEdges(0.0, 0.0, 0.0, i, j, k, ex, ey, ez, ex0, ey0, ez0)
+      #else
+        ey0 = ey(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = ey0 * B_norm
+    case('ez')
+      #ifndef debug
+        call interpFromEdges(0.0, 0.0, 0.0, i, j, k, ex, ey, ez, ex0, ey0, ez0)
+      #else
+        ez0 = ez(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = ez0 * B_norm
+    case('bx')
+      #ifndef debug
+        call interpFromFaces(0.0, 0.0, 0.0, i, j, k, bx, by, bz, bx0, by0, bz0)
+      #else
+        bx0 = bx(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = bx0 * B_norm
+    case('by')
+      #ifndef debug
+        call interpFromFaces(0.0, 0.0, 0.0, i, j, k, bx, by, bz, bx0, by0, bz0)
+      #else
+        by0 = by(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = by0 * B_norm
+    case('bz')
+      #ifndef debug
+        call interpFromFaces(0.0, 0.0, 0.0, i, j, k, bx, by, bz, bx0, by0, bz0)
+      #else
+        bz0 = bz(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = bz0 * B_norm
+    case('jx')
+      #ifndef debug
+        call interpFromEdges(0.0, 0.0, 0.0, i, j, k, jx, jy, jz, jx0, jy0, jz0)
+      #else
+        jx0 = jx(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = -jx0 * B_norm
+    case('jy')
+      #ifndef debug
+        call interpFromEdges(0.0, 0.0, 0.0, i, j, k, jx, jy, jz, jx0, jy0, jz0)
+      #else
+        jy0 = jy(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = -jy0 * B_norm
+    case('jz')
+      #ifndef debug
+        call interpFromEdges(0.0, 0.0, 0.0, i, j, k, jx, jy, jz, jx0, jy0, jz0)
+      #else
+        jz0 = jz(i, j, k)
+      #endif
+      sm_arr(i1, j1, k1) = -jz0 * B_norm
+    case('xx')
+      sm_arr(i1, j1, k1) = REAL(this_meshblock%ptr%x0 + i, 4)
+    case('yy')
+      sm_arr(i1, j1, k1) = REAL(this_meshblock%ptr%y0 + j, 4)
+    case('zz')
+      sm_arr(i1, j1, k1) = REAL(this_meshblock%ptr%z0 + k, 4)
+    case default
+      if (((fld_var(1:4) .ne. 'dens') .and. (fld_var(1:4) .ne. 'enrg')) .or.&
+         & (.not. writing_lgarrQ)) then
+        call throwError("ERROR: unrecognized `fld_vars(f)`")
+      else
+        sm_arr(i1, j1, k1) = lg_arr(i, j, k)
+      end if
+    end select
+  end subroutine selectFieldForOutput
+
 end module m_helpers
