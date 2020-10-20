62ff1541d8116a03a81899c0c7a208b48dd302a9
Hayk Hakobyan <hayk.hakopyan@gmail.com>
2020-10-20 18:30:35 -0400

    thermal plasma modeling critical temp for tabulated vs Sobol

--- src/physics/thermal_plasma.F90
+++ src/physics/thermal_plasma.F90
@@ -24,7 +24,7 @@ module m_thermalplasma
   end type maxwellian
 
   !--- PRIVATE variables -----------------------------------------!
-  real    :: t_crit = 0.1
+  real    :: t_crit = 0.01
   private :: t_crit
   !...............................................................!
 
