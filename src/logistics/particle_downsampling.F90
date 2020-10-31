53dcba0e544b343f203d907251c69dde880a42e3
Jens Mahlmann <mahlmann@princeton.edu>
2020-10-30 21:23:19 -0400

    Minor changes and testing.

--- src/logistics/particle_downsampling.F90
+++ src/logistics/particle_downsampling.F90
@@ -551,11 +551,11 @@ contains
     #endif
 
     ! take two random particles to position the new ones
-    p_ind = INT((random(dseed) * group%size + 1))
-    p = group%indices(p_ind)
-    xAi = tile%xi(p); dxA = tile%dx(p)
-    yAi = tile%yi(p); dyA = tile%dy(p)
-    zAi = tile%zi(p); dzA = tile%dz(p)
+    ! p_ind = INT((random(dseed) * group%size + 1))
+    ! p = group%indices(p_ind)
+    ! xAi = tile%xi(p); dxA = tile%dx(p)
+    ! yAi = tile%yi(p); dyA = tile%dy(p)
+    ! zAi = tile%zi(p); dzA = tile%dz(p)
 
     ! p = p_ind
     ! do while (p .eq. p_ind)
@@ -566,41 +566,41 @@ contains
     ! yBi = tile%yi(p); dyB = tile%dy(p)
     ! zBi = tile%zi(p); dzB = tile%dz(p)
 
-    xBi = tile%xi(p); dxB = tile%dx(p)
-    yBi = tile%yi(p); dyB = tile%dy(p)
-    zBi = tile%zi(p); dzB = tile%dz(p)
+    ! xBi = tile%xi(p); dxB = tile%dx(p)
+    ! yBi = tile%yi(p); dyB = tile%dy(p)
+    ! zBi = tile%zi(p); dzB = tile%dz(p)
 
-    x2 = real(xBi) + dxB
-    y2 = real(yBi) + dyB
-    z2 = real(zBi) + dzB
+    ! x2 = real(xBi) + dxB
+    ! y2 = real(yBi) + dyB
+    ! z2 = real(zBi) + dzB
 
-    ! x2 = 0.0
-    ! y2 = 0.0
-    ! z2 = 0.0
-    ! totweight = 0.0
+    x2 = 0.0
+    y2 = 0.0
+    z2 = 0.0
+    totweight = 0.0
 
-    ! do p_ind = 1, group%size
-    !   p = group%indices(p_ind)
+    do p_ind = 1, group%size
+      p = group%indices(p_ind)
 
-    !   x2 = x2 + tile%weight(p) * tile%dx(p)
-    !   y2 = y2 + tile%weight(p) * tile%dy(p)
-    !   z2 = z2 + tile%weight(p) * tile%dz(p)
+      x2 = x2 + tile%weight(p) * tile%dx(p)
+      y2 = y2 + tile%weight(p) * tile%dy(p)
+      z2 = z2 + tile%weight(p) * tile%dz(p)
 
-    !   totweight = totweight + tile%weight(p)
+      totweight = totweight + tile%weight(p)
 
-    ! enddo
+    enddo
 
-    ! xAi = tile%xi(p); dxA = x2 / totweight
-    ! yAi = tile%yi(p); dyA = y2 / totweight
-    ! zAi = tile%zi(p); dzA = z2 / totweight
+    xAi = tile%xi(p); dxA = x2 / totweight
+    yAi = tile%yi(p); dyA = y2 / totweight
+    zAi = tile%zi(p); dzA = z2 / totweight
 
-    ! xBi = tile%xi(p); dxB = x2 / totweight
-    ! yBi = tile%yi(p); dyB = y2 / totweight
-    ! zBi = tile%zi(p); dzB = z2 / totweight
+    xBi = tile%xi(p); dxB = x2 / totweight
+    yBi = tile%yi(p); dyB = y2 / totweight
+    zBi = tile%zi(p); dzB = z2 / totweight
 
-    !   x2 = x2 + real(tile%xi(p))
-    !   y2 = y2 + real(tile%yi(p))
-    !   z2 = z2 + real(tile%zi(p))
+      x2 = x2 + real(tile%xi(p))
+      y2 = y2 + real(tile%yi(p))
+      z2 = z2 + real(tile%zi(p))
 
     ! Extra current deposit
     do p_ind = 1, group%size
