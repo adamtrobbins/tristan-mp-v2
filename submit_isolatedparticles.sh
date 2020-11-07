9278cf34630add37637c84f3210b6d364d6a9c12
Jens Mahlmann <mahlmann@princeton.edu>
2020-11-07 00:06:08 -0500

    Minor changes and testing.

---
+++ submit_isolatedparticles.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH --time=02:00:00
+#SBATCH -N 1 -n 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=/scratch/gpfs/mahlmann/Tristan/tristan-sims/Isoltaed_Test_RAND/tristan-v2.out
+#SBATCH --error=/scratch/gpfs/mahlmann/Tristan/tristan-sims/Isoltaed_Test_RAND/tristan-v2.err
+
+module load intel
+module load intel-mpi
+module load hdf5/intel-16.0/intel-mpi/1.8.16
+
+time srun -N 1 -n 1 --ntasks-per-node=1  exec/tristan-mp2d -i inputs/input.2disolatedparticles -o /scratch/gpfs/mahlmann/Tristan/tristan-sims/Isoltaed_Test_RAND
