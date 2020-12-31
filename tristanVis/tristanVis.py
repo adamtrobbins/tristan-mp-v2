d7d3413973d761eb6b7c0169c17dbf8132e28f7e
haykh <hayk.hakopyan@gmail.com>
2020-12-31 03:31:09 -0500

    moved old stuff to legacy + fixed a few things

--- tristanVis/tristanVis.py
+++ tristanVis/tristanVis.py
@@ -74,7 +74,7 @@ class FieldData2D():
 
 class Simulation():
   def __init__(self,
-               root, fld_steps=None, useSlices=True, mask=None,
+               root, fld_steps=None, useSlices=False, mask=None,
                coordinateTransformation={'x': lambda x: x, 'y': lambda y: y, 'z': lambda z: z},
                extraVariables=None
               ):
@@ -141,7 +141,7 @@ class Simulation():
                         extraVariables=self._extraVariables,
                         coordinateTransformation=self._coordinateTransformation)
       self.fields.update({st: fld})
-      
+
   def loadData(self):
     self.readFiles()
     [fld.loadData() for st, fld in self.fields.items()]
@@ -506,3 +506,124 @@ class PlotGrid():
 
   def show(self):
     display(self.plotgrid)
+
+# not working atm...
+# class TwoDPlotAndSpectra:
+#   def __init__(self, x, y, z, field_data, spec_data, coordinates=None, imshow_kwargs={}, rectangle_kwargs={}, spectra_kwargs={}):
+#     self.x = x; self.y = y; self.z = z
+#     self.field_data = field_data
+#     self.spec_data = spec_data
+#     self.coordinates = coordinates
+#     self.imshow_kwargs = imshow_kwargs
+#     self.rectangle_kwargs = rectangle_kwargs
+#     self.spectra_kwargs = spectra_kwargs
+#     self.SHIFT = False
+#     self.specbins = []
+#
+#   def on_key_press(self, event):
+#     if event.key == 'shift':
+#       self.SHIFT = True
+#
+#   def on_key_release(self, event):
+#     if event.key == 'shift':
+#       self.SHIFT = False
+#
+#   def on_click(self, event):
+#     print (event.x, event.y)
+#     import matplotlib.patches as mpatches
+#     if (self.coordinates == 'xy'):
+#       xi = event.xdata; yi = event.ydata; zi = self.z.mean()
+#     elif (self.coordinates == 'xz'):
+#       xi = event.xdata; yi = self.y.mean(); zi = event.ydata
+#     elif (self.coordinates == 'yz'):
+#       xi = self.x.mean(); yi = event.xdata; zi = event.ydata
+#     else:
+#       raise ValueError('Wrong coordinate system')
+#     # add species #1 and #2
+#     xyz, sxyz, cnt1 = self.spec_data.getByCoordinate(1, (xi, yi, zi))
+#     xyz, sxyz, cnt2 = self.spec_data.getByCoordinate(2, (xi, yi, zi))
+#     cnt = cnt1 + cnt2
+#     if (self.coordinates == 'xy'):
+#       x0, y0 = (xyz[0], xyz[1])
+#       sx, sy = (sxyz[0], sxyz[1])
+#     elif (self.coordinates == 'xz'):
+#       x0, y0 = (xyz[0], xyz[2])
+#       sx, sy = (sxyz[0], sxyz[2])
+#     elif (self.coordinates == 'yz'):
+#       x0, y0 = (xyz[1], xyz[2])
+#       sx, sy = (sxyz[1], sxyz[2])
+#     else:
+#       raise ValueError('Wrong coordinate system')
+#     bns = np.copy(self.spec_data.ebins)
+#     if not self.spectra_kwargs['smooth'] is None:
+#       from scipy.ndimage import gaussian_filter1d as smooth
+#       cnt = smooth(np.copy(cnt / bns), self.spectra_kwargs['smooth'])
+#     else:
+#       cnt = np.copy(cnt / bns)
+#     if ((not self.SHIFT) or (self.specbins == [])):
+#       self.specbins = [(x0, y0)]
+#       self.spec_x = bns
+#       self.spec_y = cnt
+#       self.spec.set_xdata(self.spec_x)
+#       self.spec.set_ydata(self.spec_y)
+#       while len(self.fig.axes[0].patches) != 0:
+#         self.fig.axes[0].patches[0].remove()
+#       self.fig.axes[0].add_patch(mpatches.Rectangle((x0, y0), sx, sy, fc='None', zorder=100, **self.rectangle_kwargs))
+#     else:
+#       if (not (x0, y0) in self.specbins):
+#         self.specbins.append((x0, y0))
+#         self.spec_x = bns
+#         try:
+#           self.spec_y += cnt
+#         except:
+#           self.spec_y = cnt
+#         self.spec.set_xdata(self.spec_x)
+#         self.spec.set_ydata(self.spec_y)
+#         self.fig.axes[0].add_patch(mpatches.Rectangle((x0, y0), sx, sy, fc='None', zorder=100, **self.rectangle_kwargs))
+#       else:
+#         self.specbins.remove((x0, y0))
+#         self.spec_y -= cnt
+#         self.spec.set_xdata(self.spec_x)
+#         self.spec.set_ydata(self.spec_y)
+#         for rect in self.fig.axes[0].patches:
+#           if (rect.get_xy() == (x0, y0)):
+#             rect.remove()
+#             break
+#
+#     self.fig.canvas.draw()
+#     self.fig.canvas.flush_events()
+#
+#   def plot(self, figsize=(12, 4)):
+#     import matplotlib.pyplot as plt
+#     import matplotlib.patches as mpatches
+#     from mpl_toolkits.axes_grid1 import make_axes_locatable
+#     self.fig = plt.figure(figsize=figsize)
+#     if (self.coordinates is None):
+#       if self.x.min() == self.x.max():
+#         self.coordinates = 'yz'
+#       elif self.y.min() == self.y.max():
+#         self.coordinates = 'xz'
+#       elif self.z.min() == self.z.max():
+#         self.coordinates = 'xy'
+#     ax_fld = self.fig.add_subplot(121)
+#     im = ax_fld.imshow(self.field_data, origin='lower', **self.imshow_kwargs)
+#     ax_fld.set_xlabel(self.coordinates[0])
+#     ax_fld.set_ylabel(self.coordinates[1])
+#     ax_spec = self.fig.add_subplot(122)
+#     self.spec, = ax_spec.plot([], [])
+#     ax_spec.set_xlim(*self.spectra_kwargs['xlim'])
+#     ax_spec.set_ylim(*self.spectra_kwargs['ylim'])
+#     ax_spec.set_xscale('log')
+#     ax_spec.set_yscale('log')
+#     ax_spec.set_xlabel(r'$\gamma - 1$')
+#     ax_spec.set_ylabel(r'$(\gamma - 1)f(\gamma)$')
+#     divider = make_axes_locatable(ax_fld)
+#     cax = divider.append_axes("right", size="5%", pad=0.05)
+#     plt.colorbar(im, cax=cax)
+#     rect = mpatches.Rectangle((0, 0), 0, 0, fc='None', zorder=100, **self.rectangle_kwargs)
+#     ax_fld.add_patch(rect)
+#     cid = self.fig.canvas.mpl_connect('button_press_event', self.on_click)
+#     self.fig.canvas.mpl_connect('key_press_event', self.on_key_press)
+#     self.fig.canvas.mpl_connect('key_release_event', self.on_key_release)
+#     plt.tight_layout()
+#     plt.show()
