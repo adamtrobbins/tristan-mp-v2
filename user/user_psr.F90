86ae01ea452e69ebc5eb2e10db0991e81479ab34
haykh <hayk.hakopyan@gmail.com>
2021-01-09 15:46:53 -0500

    configure flags added

--- user/user_psr.F90
+++ user/user_psr.F90
@@ -1,5 +1,10 @@
 #include "../src/defs.F90"
 
+! Configuration for this userfile:
+! ```
+!   $ python configure.py -3d --user=user_psr -slb --gca=2 --radiation=sync -debug -absorb
+! ```
+
 module m_userfile
   use m_globalnamespace
   use m_aux
