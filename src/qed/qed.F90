0452a5c4fb32693450f8547a8f491fb7becb2fd4
dgroselj <daniel.grosel@gmail.com>
2022-09-27 15:41:19 -0400

    compton for high scattering freq

--- src/qed/qed.F90
+++ src/qed/qed.F90
@@ -95,8 +95,6 @@ contains
     call getInput('compton', 'el_recoil', Compton_el_recoil, .true.)
     call getInput('compton', 'Thomson_lim', Thomson_lim, 1d-6)
     call getInput('compton', 'nph_over_ne', Compton_nph_over_ne, 1.0)
-    call getInput('compton', 'clone_sets', Compton_clone_sets, .true.)
-    call getInput('compton', 'cool_el', Compton_cool_el, .false.)
 
     do s = 1, nspec
       write (var_name, "(A7,I1)") "compton", s
