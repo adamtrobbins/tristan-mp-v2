16f15ce475829c713f1ceb14f2dcde665f2acfb3
adamtrobbins <adamtrobbins@gmail.com>
2026-03-15 15:15:02 -0400

    fixed zero index for ez, seems to work well

--- user/user_rec_dynez.F90
+++ user/user_rec_dynez.F90
@@ -315,14 +315,14 @@ contains
     ! left third (after BC region)
     do ii = int(x1min), nx_third - 1
       do jj = 0, this_meshblock % ptr % sy - 1
-        ez_sum_both = ez_sum_both + ez(ii, jj, 1)
+        ez_sum_both = ez_sum_both + ez(ii, jj, 0)
         ez_cnt_both = ez_cnt_both + 1
       end do
     end do
     ! right third (before BC region)
     do ii = sx_loc - nx_third, int(x1max) - 1
       do jj = 0, this_meshblock % ptr % sy - 1
-        ez_sum_both = ez_sum_both + ez(ii, jj, 1)
+        ez_sum_both = ez_sum_both + ez(ii, jj, 0)
         ez_cnt_both = ez_cnt_both + 1
       end do
     end do
